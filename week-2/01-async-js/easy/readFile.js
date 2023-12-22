fd1d1afa04e51ea94a2cc2cb9140d3cdb9e5fa8a
Akshansh <s.akshansh01@gmail.com>
2023-12-22 23:44:44 +0530

    3rd easy task

--- week-2/01-async-js/easy/readFile.js
+++ week-2/01-async-js/easy/readFile.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 
 fs.readFile('./3-read-from-file.md', 'utf8', (err, data) => {
+    for (let i=0;i<1000000000;i++){
+        continue;
+    }
     if (err) {
         console.error('Error reading the file', err);
         return;
     }
     console.log(data);
 });
+
