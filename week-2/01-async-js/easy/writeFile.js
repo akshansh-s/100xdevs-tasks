0297ffe1626833dbefcd5ec84f376443e2b19b7a
Akshansh <s.akshansh01@gmail.com>
2024-01-02 19:00:59 +0530

    3 hard tasks

--- week-2/01-async-js/easy/writeFile.js
+++ week-2/01-async-js/easy/writeFile.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const data=" Hello write this to file"
 
-fs.writeFile('./3-read-from-file.md',data , 'utf8', (err, data) => {
+fs.writeFile('./3-read-from-file.md',data , 'utf8', (err) => {
     if (err) {
         console.error('Error writing the file', err);
         return;
--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,12 @@
 */
 
 function wait(n) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, n * 1000);
+    });
+
 }
 
 module.exports = wait;
+
+wait(3).then(() => console.log('3 seconds have passed'));
