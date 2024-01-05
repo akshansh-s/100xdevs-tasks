baed59b3d6d98399677408f906f3d4f5cdaf34f4
Akshansh <s.akshansh01@gmail.com>
2024-01-05 23:28:51 +0530

    3rd week easy task

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req,res,next)=>{
+  const userId= req.headers["user-id"];
+
+  if(!userId){
+    return res.status(400).json({error:"User id not provided"});
+  }
+
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 1;
+  } else if (numberOfRequestsForUser[userId] >= 5) {
+    return res.status(404).json({ error: 'Request limit exceeded' });
+  } else {
+    numberOfRequestsForUser[userId]++;
+  }
+  next();
+})
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
