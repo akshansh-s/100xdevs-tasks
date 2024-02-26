a740880b9c2cc29faac6a9c92e2c465145342858
Akshansh <s.akshansh01@gmail.com>
2024-02-26 23:55:08 +0530

    assignment3

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,7 @@
 const { Admin }= require("../db/index")
 const jwt= require("jsonwebtoken")
 const JWT_SECRET=require("../config")
-// Middleware for handling auth
+
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
